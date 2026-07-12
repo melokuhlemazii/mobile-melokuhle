32a173e0db37abb5de9d130248078f3997e11dd0
melokuhlemazii <melomazibuko8@gmail.com>
2026-07-12 08:19:08 +0200

    First Commit

---
+++ src/test/java/StartNdosiMobileApp.java
@@ -0,0 +1,38 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+public class StartNdosiMobileApp {
+
+    public static AndroidDriver driver;
+
+    @BeforeTest
+    public void setup() throws MalformedURLException {
+        UiAutomator2Options options = new UiAutomator2Options()
+                .setPlatformName("Android")
+                .setAutomationName("UiAutomator2")
+                .setApp(System.getProperty("user.dir") + "/src/main/Apps/app-qa-release.apk");
+        driver = new AndroidDriver(URI.create("http://127.0.0.1:4723/").toURL(), options);
+    }
+
+    @Test
+    public void launchNdosiApp() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//android.widget.Button")).click();
+    }
+
+    @AfterTest
+    public void quitApp() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
